edfa540821dbb1ae90f124dffa2851146000634a
RSaravanan17 <rithvik.saravanan17@gmail.com>
2017-07-27 14:16:36 -0500

    Changing to port 80 using devServer

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,14 @@ var webpack = require("webpack");
 var path = require("path");
 module.exports = {
   entry: {
-    'webpack-dev-server/client?http://0.0.0.0:80',
+    /*'webpack-dev-server/client?http://0.0.0.0:80',
+     config.paths.demo*/
     app: ["./blackjack.js"]
+  },
+  devServer: {
+    inline: true,
+    host: '0.0.0.0',
+    port: 80
   },
   output: {
     path: path.resolve(__dirname, "build"),
