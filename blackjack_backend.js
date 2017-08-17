47f30b9884a9762a4b5ee6c93fda585a53e5b72e
RSaravanan17 <rithvik.saravanan17@gmail.com>
2017-08-17 14:53:41 -0500

    Including single browser version and commenting multiple browser version

--- blackjack_backend.js
+++ blackjack_backend.js
@@ -1,4 +1,4 @@
-var blackjack_backend = (function() {
+/*var blackjack_backend = (function() {
   var currentPlayer = [];
   var deck = [];
   var dealerScore = [];
@@ -176,12 +176,12 @@ var blackjack_backend = (function() {
     if (bothStand) {
       gameOver[sessId] = true;
       if (dealerScore[sessId] > playerScore[sessId]) {
-        gameState[sessId] = "Since you and the dealer both took a stand, the game is over. The dealer's final score is " + dealerScore[sessId] + " and your final score is " + playerScore + ". You lose.";
+        gameState[sessId] = "Since you and the dealer both took a stand, the game is over. The dealer's final score is " + dealerScore[sessId] + " and your final score is " + playerScore[sessId] + ". You lose.";
       } else if (playerScore[sessId] > dealerScore[sessId]) {
-        gameState[sessId] = "Since you and the dealer both took a stand, the game is over. The dealer's final score is " + dealerScore[sessId] + " and your final score is " + playerScore + ". Congratulations, you win!";
+        gameState[sessId] = "Since you and the dealer both took a stand, the game is over. The dealer's final score is " + dealerScore[sessId] + " and your final score is " + playerScore[sessId] + ". Congratulations, you win!";
         win[sessId] = true;
       } else if (playerScore[sessId] === dealerScore[sessId]) {
-        gameState[sessId] = "Since you and the dealer both took a stand, the game is over. The dealer's final score is " + dealerScore[sessId] + " and your final score is " + playerScore + ". The game ends in a tie.";
+        gameState[sessId] = "Since you and the dealer both took a stand, the game is over. The dealer's final score is " + dealerScore[sessId] + " and your final score is " + playerScore[sessId] + ". The game ends in a tie.";
         tie[sessId] = true;
       }
     } else {
@@ -279,17 +279,11 @@ var blackjack_backend = (function() {
     let username = url.slice(url.indexOf('username=') + 9);
 
     let cardNums = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"];
-    deck.push({sessId: []});
     deck[sessId] = [];
-    dealerScore.push({sessId: 0});
     dealerScore[sessId] = 0;
-    playerScore.push({sessId: 0});
     playerScore[sessId] = 0;
-    dealerHand.push({sessId: []});
     dealerHand[sessId] = [];
-    playerHand.push({sessId: []});
     playerHand[sessId] = [];
-    gameOver.push({sessId: false});
     gameOver[sessId] = false;
     for (var i = 0; i < cardNums.length; i++) {
       deck[sessId].push(cardNums[i] + " of Clubs", cardNums[i] + " of Spades", cardNums[i] + " of Diamonds", cardNums[i] + " of Hearts");
@@ -300,6 +294,302 @@ var blackjack_backend = (function() {
       playerScore: playerScore[sessId],
       gameOver: gameOver[sessId],
       gameState: gameState[sessId]
+    };
+  }
+})();*/
+
+var blackjack_backend = (function() {
+  var currentPlayer;
+  var deck;
+  var dealerScore;
+  var playerScore;
+  var dealerHand;
+  var playerHand;
+  var gameOver;
+  var gameState;
+
+  var http = require('http');
+  var url = require('url');
+  var cors = require('cors');
+  var express = require('express');
+
+  var firebase = require('firebase');
+  var config = {
+    apiKey: "AIzaSyBX9CyTmSz0sDhMzCd9zINumBTIfr_O1X8",
+    authDomain: "blackjack-5a244.firebaseapp.com",
+    databaseURL: "https://blackjack-5a244.firebaseio.com",
+    projectId: "blackjack-5a244",
+    storageBucket: "blackjack-5a244.appspot.com",
+    messagingSenderId: "841464784637"
+  };
+  firebase.initializeApp(config);
+  var db = firebase.database();
+  var ref = db.ref('server/saving-data/blackjack');
+  var usersRef = ref.child('users');
+
+  var server = http.createServer(function(req, res) {
+    var parsedUrl = url.parse(req.url, true);
+    var result;
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    if (/^\/api\/login/.test(req.url)) {
+      result = login(req.url);
+    } else if (/^\/api\/init/.test(req.url)) {
+      result = initGame(req.url);
+    } else if (/^\/api\/hit/.test(req.url)) {
+      result = moveHit(req.url);
+    } else if (/^\/api\/stand/.test(req.url)) {
+      result = moveStand(req.url);
+    } else if (/^\/api\/updatePlayers/.test(req.url)) {
+      result = updatePlayerList();
+    }
+
+    if (result) {
+      res.writeHead(200, {
+        'Content-Type': 'application/json'
+      });
+      res.end(JSON.stringify(result));
+    } else {
+      res.writeHead(404);
+      res.end();
+    }
+  });
+
+  server.on('listening', function() {
+    console.log('ok, server is running');
+  });
+
+  server.listen(3000);
+
+  function login(url) {
+    let sessId = JSON.stringify(new Date().getTime());
+    let statement = "";
+    let username = url.slice(url.indexOf('username=') + 9, url.indexOf('&'));
+    let password = url.slice(url.indexOf('password=') + 9);
+    let playerFound = false;
+
+    usersRef.on("child_added", function(data, prevChildKey) {
+      let playerInfo = [];
+      playerInfo = data.val();
+      if (playerInfo.un === username) {
+        playerFound = true;
+        currentPlayer = data.val();
+      }
+      if (playerFound) {
+        if (playerInfo.pw === password) {
+          statement = "Welcome back to Blackjack! You have " + playerInfo.win + " win(s) and " + playerInfo.loss + " loss(es).";
+        }
+      }
+    }, function(err) {
+      console.log(err);
+    });
+
+    if (!playerFound) {
+      usersRef.update({
+        [username]: {
+          'un': username,
+          'pw': password,
+          'win': 0,
+          'loss': 0
+        }
+      });
+      statement = "Welcome to Blackjack! You can choose to 'hit' or 'stand' in the red area and you can see your hand in the blue area.";
+    }
+
+    return {
+      user: username,
+      statement: statement,
+      sessId: sessId
+    };
+  }
+
+  function updatePlayerList() {
+    let listOfPlayers = [];
+
+    usersRef.on("child_added", function(data, prevChildKey) {
+      let playerInfo = data.val();
+      listOfPlayers.push({
+        un: playerInfo.un,
+        win: playerInfo.win,
+        loss: playerInfo.loss
+      });
+    }, function(err) {
+      console.log(err);
+    });
+
+    return {
+      listOfPlayers: listOfPlayers
+    }
+  }
+
+  function scoreWithoutAce(currentDeck, score) {
+    score = 0;
+    for (var i = 0; i < currentDeck.length; i++) {
+      if (currentDeck[i].includes("Two")) {
+        score += 2;
+      } else if (currentDeck[i].includes("Three")) {
+        score += 3;
+      } else if (currentDeck[i].includes("Four")) {
+        score += 4;
+      } else if (currentDeck[i].includes("Five")) {
+        score += 5;
+      } else if (currentDeck[i].includes("Six")) {
+        score += 6;
+      } else if (currentDeck[i].includes("Seven")) {
+        score += 7;
+      } else if (currentDeck[i].includes("Eight")) {
+        score += 8;
+      } else if (currentDeck[i].includes("Nine")) {
+        score += 9;
+      } else if (currentDeck[i].includes("Ten") || currentDeck[i].includes("Jack") || currentDeck[i].includes("Queen") || currentDeck[i].includes("King")) {
+        score += 10;
+      }
+    }
+    return score;
+  }
+
+  function scoreWithAce(currentDeck, score) {
+    for (var i = 0; i < currentDeck.length; i++) {
+      if (currentDeck[i].includes("Ace")) {
+        if (score <= 10) {
+          score += 11;
+        } else {
+          score += 1;
+        }
+      }
+    }
+    return score;
+  }
+
+  function updateScore(username, bothStand, sessId) {
+    gameState = "";
+    let tie = false;
+    let win = false;
+
+    dealerScore = scoreWithAce(dealerHand, scoreWithoutAce(dealerHand, dealerScore));
+    playerScore = scoreWithAce(playerHand, scoreWithoutAce(playerHand, playerScore));
+
+    if (bothStand) {
+      gameOver = true;
+      if (dealerScore > playerScore) {
+        gameState = "Since you and the dealer both took a stand, the game is over. The dealer's final score is " + dealerScore + " and your final score is " + playerScore + ". You lose.";
+      } else if (playerScore > dealerScore) {
+        gameState = "Since you and the dealer both took a stand, the game is over. The dealer's final score is " + dealerScore + " and your final score is " + playerScore + ". Congratulations, you win!";
+        win = true;
+      } else if (playerScore === dealerScore) {
+        gameState = "Since you and the dealer both took a stand, the game is over. The dealer's final score is " + dealerScore + " and your final score is " + playerScore + ". The game ends in a tie.";
+        tie = true;
+      }
+    } else {
+      if (playerScore > 21) {
+        gameState = "Your score of " + playerScore + " is over 21 so the game is over. You lost.";
+        gameOver = true;
+      } else if (dealerScore > 21) {
+        gameState = "The dealer has a bust because his score of " + dealerScore + " is over 21. Congratulations, you win!";
+        gameOver = true;
+        win = true;
+      } else if (dealerScore === 21 && playerScore === 21) {
+        gameState = "Since you and the dealer both have a score of 21, the game ends in a tie.";
+        gameOver = true;
+        tie = true;
+      } else if (dealerScore === 21) {
+        gameState = "The dealer has a score of exactly 21. You lost.";
+        gameOver = true;
+      } else if (playerScore === 21) {
+        gameState = "Since your score is exactly 21, you win!.";
+        gameOver = true;
+        win = true;
+      } else {
+        gameState = "Continue playing.";
+        gameOver = false;
+      }
+    }
+    if (gameOver[sessId] && !tie[sessId]) {
+      if (win[sessId]) {
+        usersRef.child(username).update({
+          'win': currentPlayer.win + 1
+        });
+      } else {
+        usersRef.child(username).update({
+          'loss': currentPlayer.loss + 1
+        });
+      }
+    }
+  }
+
+  function randomCard(currentDeck, sessId) {
+    var index = Math.floor(Math.random() * currentDeck.length);
+    var newCard = currentDeck[index];
+    deck.splice(index, 1);
+    return newCard;
+  }
+
+  function deal(username, sessId) {
+    dealerHand.push(randomCard(deck, sessId), randomCard(deck, sessId));
+    playerHand.push(randomCard(deck, sessId), randomCard(deck, sessId));
+    updateScore(username, false, sessId);
+  }
+
+  function moveHit(url) {
+    let sessId = url.slice(url.indexOf('sessId=') + 7, url.indexOf('&'));
+    let username = url.slice(url.indexOf('username=') + 9);
+    playerHand.push(randomCard(deck, sessId));
+    updateScore(username, false, sessId);
+    return {
+      gameOver: gameOver,
+      gameState: gameState,
+      playerHand: playerHand,
+      playerScore: playerScore
+    };
+  }
+
+  function moveStand(url) {
+    let sessId = url.slice(url.indexOf('sessId=') + 7, url.indexOf('&'));
+    let username = url.slice(url.indexOf('username=') + 9);
+    let dealerHit = [];
+    dealerHit = 0;
+    while (dealerScore < 17) {
+      dealerHit++;
+      dealerHand.push(randomCard(deck, sessId));
+      updateScore(username, false, sessId);
+      if (gameOver && dealerScore > 21) {
+        return {
+          gameOver: gameOver,
+          dealerHit: dealerHit,
+          gameState: gameState,
+          bust: true
+        };
+      }
+    }
+    updateScore(username, true, sessId);
+    return {
+      gameOver: gameOver,
+      dealerHit: dealerHit,
+      gameState: gameState,
+      bust: false
+    };
+  }
+
+  function initGame(url) {
+    let sessId = url.slice(url.indexOf('sessId=') + 7, url.indexOf('&'));
+    let username = url.slice(url.indexOf('username=') + 9);
+
+    let cardNums = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"];
+    deck = [];
+    dealerScore = 0;
+    playerScore = 0;
+    dealerHand = [];
+    playerHand = [];
+    gameOver = false;
+    for (var i = 0; i < cardNums.length; i++) {
+      deck.push(cardNums[i] + " of Clubs", cardNums[i] + " of Spades", cardNums[i] + " of Diamonds", cardNums[i] + " of Hearts");
+    }
+    deal(username, sessId);
+    return {
+      playerHand: playerHand,
+      playerScore: playerScore,
+      gameOver: gameOver,
+      gameState: gameState
     };
   }
 })();
