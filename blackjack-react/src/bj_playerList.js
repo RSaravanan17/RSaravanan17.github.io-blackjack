ca346d2a07657530448e3eb221d984fdc8e533a9
RSaravanan17 <rithvik.saravanan17@gmail.com>
2017-08-21 16:04:17 -0500

    Changed ordered list code to React component

--- blackjack-react/src/bj_playerList.js
+++ blackjack-react/src/bj_playerList.js
@@ -4,10 +4,28 @@ import ReactDOM from 'react-dom';
 class PlayerList extends React.Component {
   render() {
     if (this.props.listOfPlayers.length > 0) {
+      let orderPlayers = [];
+      let localPlayers = this.props.listOfPlayers;
+      let tempWin = -1;
+      let tempIndex = -1;
+      while (localPlayers.length > 0) {
+        for (var i = 0; i < localPlayers.length; i++) {
+          if (tempWin < localPlayers[i].win) {
+            tempWin = localPlayers[i].win;
+            tempIndex = i;
+          }
+        }
+        orderPlayers.push({
+          un: localPlayers[tempIndex].un,
+          win: localPlayers[tempIndex].win,
+          loss: localPlayers[tempIndex].loss
+        });
+        delete localPlayers[tempIndex];
+      }
       return (
         <div>
         {
-          this.props.listOfPlayers.map((player, i) => {
+          orderPlayers.map((player, i) => {
             return <li key={i} id={player.un}>User: {player.un} | Won: {player.win} | Lost: {player.loss}</li>;
           })
         }
